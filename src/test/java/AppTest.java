2fd6d3e8848add732e94eac7cb8e48a843a4259f
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-05-17 19:08:34 -0700

    add venue.vtl page with path to it, test pasess

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -84,70 +84,15 @@ public class AppTest extends FluentTest {
    assertThat(pageSource()).contains("Rose Garden");
  }
 
+ @Test
+ public void venueShowPageDisplaysName() {
+  Venue testVenue = new Venue("Rose Garden");
+  testVenue.save();
+  String url = String.format("http://localhost:4567/venue/%d", testVenue.getId());
+  goTo(url);
+  assertThat(pageSource()).contains("Rose Garden");
+}
 
 
-//
-//   @Test
-//   public void ingredientAddedToRecipePage() {
-//     Recipe newRecipe = new Recipe("Pie", "Bake a pie", 5);
-//     newRecipe.save();
-//     Ingredient newIngredient = new Ingredient("Flour");
-//     newIngredient.save();
-//     String url = String.format("http://localhost:4567/recipes/%d", newRecipe.getId());
-//     goTo(url);
-//     assertThat(pageSource()).contains("Flour");
-//   }
-//
-//   @Test
-//   public void createCategory() {
-//     goTo("http://localhost:4567/");
-//     fill("#category").with("Dinner");
-//     submit("#save-category");
-//     assertThat(pageSource()).contains("Dinner");
-//   }
-//
-//   @Test
-//   public void displayCategoryPage() {
-//     Category newCategory = new Category ("Dinner");
-//     newCategory.save();
-//     String url = String.format("http://localhost:4567/categories/%d", newCategory.getId());
-//     goTo(url);
-//     assertThat(pageSource()).contains("Dinner");
-//   }
-//
-//   @Test
-//   public void deleteCategory(){
-//     Category newCategory = new Category ("Dinner");
-//     newCategory.save();
-//     String url = String.format("http://localhost:4567/categories/%d", newCategory.getId());
-//     goTo(url);
-//     submit("#delete");
-//     goTo(url);
-//     assertThat(pageSource()).contains("$category.getName()");
-//   }
-//
-//   @Test
-//   public void deleteRecipe(){
-//     Recipe newRecipe = new Recipe("Pirojki", "Bake a pirojki", 5);
-//     newRecipe.save();
-//     String url = String.format("http://localhost:4567/recipes/%d", newRecipe.getId());
-//     goTo(url);
-//     submit("#delete");
-//     goTo(url);
-//     assertThat(pageSource()).contains("$recipe.getTitle()");
-//   }
-//
-//   @Test
-//   public void updateRecipe () {
-//     Recipe newRecipe = new Recipe("Pirojki", "Bake a pirojki", 5);
-//     newRecipe.save();
-//     String url = String.format("http://localhost:4567/recipes/%d", newRecipe.getId());
-//     goTo(url);
-//     fill("#name").with("Potato");
-//     fill("#recipe").with("Buy one");
-//     fill("#rating").with("10");
-//     submit("#update");
-//     goTo(url);
-//     assertThat(pageSource()).contains("Potato");
-  // }
+
 }
